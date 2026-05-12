ead9233c08656e3b975b108525dad87059121c2d
G. Kiran Kumar <gkirankumar@meilgroup2.onmicrosoft.com>
2026-05-12 12:43:54 +0530

    Initial taskflow app

--- app/database/db.py
+++ app/database/db.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://postgres:ajay123@host.docker.internal:5432/taskdb"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "sqlite:///./taskflow.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
