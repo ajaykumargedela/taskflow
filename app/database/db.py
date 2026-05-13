7855c6bba1459339e0f2a754e4c715f4521c2671
Ajay Kumar Gedela <ajaykumargedela2002@gmail.com>
2026-05-13 13:16:01 +0530

    fix sqlite database for cloud run

--- app/database/db.py
+++ app/database/db.py
@@ -1,26 +1,12 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import declarative_base, sessionmaker
-
-# DATABASE_URL = "sqlite:///./taskflow.db"
-
-# engine = create_engine(
-#     DATABASE_URL,
-#     connect_args={"check_same_thread": False}
-# )
-
-# SessionLocal = sessionmaker(
-#     autocommit=False,
-#     autoflush=False,
-#     bind=engine
-# )
-
-# Base = declarative_base()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://postgres:ajay123@localhost:5432/taskflow"
+DATABASE_URL = "sqlite:///./taskflow.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
